a3ea54e64f8ea959f0b4cd3490ce282995bc2f9f
napier40401624 <40401624@napier.ac.uk.com>
2018-04-04 12:25:43 +0630

    delete country success

--- server.py
+++ server.py
@@ -1,56 +1,59 @@
 from flask import Flask, render_template
 import json
 
-w = json.load(open("worldl.json"))
-for c in w:
-	c['tld'] = c['tld'][1:]
-page_size = 20
+w=json.load(open("worldl.json"))
+
 app = Flask(__name__)
+for c in w:
+    c['tld']=c['tld'][1:]
+page_size=20
+app=Flask(__name__)
 
 @app.route('/')
 def mainPage():
-	return render_template('index.html',
-		w = w[0:page_size])
-
-@app.route('/begin/<b>')
-def beginPage(b):
-	bn = int(b)
-	return render_template('index.html',
-		w = w[bn:bn+page_size],
-		page_number = bn,
-		page_size = page_size
-		)
+    return render_template('index.html',
+        w=w[0:page_size],
+        page_size=page_size,
+        page_number=0
+        )     
+                           
 
 @app.route('/continent/<a>')
 def continentPage(a):
-	cl = [c for c in w if c['continent']==a]
-	return render_template(
-		'continent.html',
-		length_of_cl = len(cl),
-		cl = cl,
-		a = a
-		)
-
-@app.route('/country/<i>')
-def countryPage(i):
-	return render_template(
-		'country.html',
-		c = w[int(i)])
-
+    cl=[c['name']for c in w if c['continent']==a]
+    return render_template(
+            'continent.html',
+            length_of_cl=len(cl),
+            cl=cl,
+            a=a
+            )
+    
 @app.route('/countryByName/<n>')
 def countryByNamePage(n):
-	c = None
-	for x in w:
-		if x['name'] == n:
-			c = x
-	return render_template(
-		'country.html',
-		c = c)
+    c=None
+    for x in w:
+        if x['name']==n:
+            c=x
+    return render_template(
+        'country.html',
+        c=c)
+                        
+@app.route('/country/<i>')
+def countryPage(i):
+    return render_template('country.html', c=w[int(i)])
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+@app.route('/delete/<n>')
+def deleteCountry(n):
+    i=0
+    for c in w:
+        if c['name']==n:
+            break
+        i=i+1
+    del w[i]
+    return render_template('index.html',
+        w=w[0:page_size],
+        page_size=page_size,
+        page_number=0
+        )
 
-
-
-
-
-
+app.run(host='0.0.0.0',port=8080,debug=True)
